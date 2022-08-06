ed17b83964076de1b2a326db6e15af99789c2d38
dimas145 <13518069@std.stei.itb.ac.id>
2022-08-06 22:31:31 +0700

    init gradetablecsv plugin

---
+++ lang/en/gradeexport_gradetablecsv.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradeexport_gradetablecsv', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   gradeexport_gradetablecsv
+ * @copyright 2022, Dimas 13518069@std.stei.itb.ac.id
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['eventgradeexported'] = 'Autograding feedback table grade exported';
+$string['pluginname'] = 'Autograding feedback table file';
+$string['privacy:metadata'] = 'The autograding feedback table file grade export plugin does not store any personal data.';
+$string['timeexported'] = 'Last downloaded from this course';
+$string['gradetablecsv:publish'] = 'Publish autograding feedback grade table export';
+$string['gradetablecsv:view'] = 'Use autograding feedback grade table export';
---
+++ version.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the version information for the export grade table csv  plugin
+ *
+ * @package   gradeexport_gradetablecsv
+ * @copyright 2022, Dimas 13518069@std.stei.itb.ac.id
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'gradeexport_gradetablecsv';
+$plugin->version = 2022061500;
+$plugin->requires = 2021051700;
